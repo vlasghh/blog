45cec78be2b91051525846d00df58d4a5ad715f4
vlasghh <vlasghh@gmail.com>
2020-01-05 19:10:06 +0300

    add bye roue

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/goodbye')
+def goodbye_world():
+    return 'Goodbye, World!'
